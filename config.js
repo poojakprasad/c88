51767cfb606263134c4db1879aa2c6890f19e0e1
Pooja <pooja.prasad66@gmail.com>
2022-06-02 12:38:39 +0530

    new

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAwRCVvv5cToN1CLQ8tUm6BMqaLx5PlXFg",
+  authDomain: "storytelling-6971c.firebaseapp.com",
+  databaseURL: "https://storytelling-6971c-default-rtdb.firebaseio.com",
+  projectId: "storytelling-6971c",
+  storageBucket: "storytelling-6971c.appspot.com",
+  messagingSenderId: "405638988774",
+  appId: "1:405638988774:web:c569991638a3dd00b80c27"
+};
